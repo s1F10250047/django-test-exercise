1bbf2d103b4eb2581e690f8af6b5920c3e498819
s1f10250047 <s1f102500472@iniad.org>
2026-06-25 16:03:08 +0900

    あ

--- todo/tests.py
+++ todo/tests.py
@@ -3,6 +3,10 @@ from django.utils import timezone
 from datetime import datetime
 from todo.models import Task
 
+class SampleTestCase(TestCase):
+    def test_sample1(self):
+        self.assertEqual(1 + 2, 3)
+
 class TaskModelTestCase(TestCase):
     def test_create_task1(self):
         due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
@@ -29,6 +33,21 @@ class TaskModelTestCase(TestCase):
         task = Task(title='task1', due_at=due)
         task.save()
 
+        self.assertFalse(task.is_overdue(current))
+
+    def test_is_overdue_past(self):
+        due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
+        current = timezone.make_aware(datetime(2024, 7, 1, 0, 0, 0))
+        task = Task(title='task2', due_at=due)
+        task.save()
+        
+        self.assertTrue(task.is_overdue(current))
+
+    def test_is_overdue_none(self):
+        task = Task(title='task3', due_at=None)
+        task.save()
+        current = timezone.make_aware(datetime(2024, 7, 1, 0, 0, 0))
+        
         self.assertFalse(task.is_overdue(current))
 
 class TodoViewTestCase(TestCase):
